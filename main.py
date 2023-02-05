ab7cc25c9da3a3cb90d9c3a2046dbdfda9e08a84
makers92 <124090937+makers92@users.noreply.github.com>
2023-02-05 15:00:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from fastapi import FastAPI
+import openai
+#importing packages
+
+app=FastAPI()
+#calling FastAPI
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(os.getenv("OPENAI_API_KEY"))
+#retrieving the API key
+
+def get_answer(phrase):
+  #defining our function & its parameters
+  prompt=f"""
+  Translate the following phrase into corporate jargon: {phrase}
+  """
+  completion = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0,
+    max_tokens=150,
+    top_p=0.25,
+    frequency_penalty=1,
+    presence_penalty=1,
+    #request body for completions endpoint
+  )
+  return completion.choices[0].text
+#returns the API result in text
+
+@app.get("/corporateJargon/{phrase}")
+async def translate_phrase(phrase):
+    return {"translation": get_answer(phrase)}
+#Returns our get_answer function to FastAPI
